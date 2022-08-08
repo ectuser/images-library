623ceb793085b040bc332169052c8b6b5c59a3db
Ivan Menshchikov <ivan_menshchikov@vanguard.com>
2022-08-08 13:56:09 +0200

    task-8 - structured the future app

---
+++ src/app/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const modules = [MatToolbarModule];
+
+@NgModule({
+  imports: [...modules],
+  exports: [...modules],
+})
+export class MaterialModule {}
