c7b5200fca85ac0a94a69a467275163594826751
Ivan Menshchikov <ivan_menshchikov@vanguard.com>
2022-08-08 14:54:40 +0200

    task-12 - added header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material.module';
 import { LayoutComponent } from './layout/layout.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, HeaderComponent],
   imports: [CommonModule, MaterialModule, RouterModule],
   exports: [LayoutComponent],
 })
