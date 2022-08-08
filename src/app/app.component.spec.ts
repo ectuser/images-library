3cee5082a84daba89db7b635720e83df92dab118
Ivan Menshchikov <ivan_menshchikov@vanguard.com>
2022-08-08 11:50:26 +0200

    task-2 - added lint-staged

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'images'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('images');
+    expect(app.appTitle).toEqual('images');
   });
 
   it('should render title', () => {
