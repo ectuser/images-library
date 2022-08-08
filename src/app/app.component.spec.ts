42d001d3110d1f93e7a4d0a932806180835be76b
Ivan Menshchikov <m.ectuser@gmail.com>
2022-08-08 12:07:27 +0200

    Task 2/configure utils for the project (#3)
    * task-2 - added eslint and plugins for typescript and angular
    
    * task-2 - added prettier and config for it
    
    * task-2 - added lint-staged
    
    * task-2 - added husky
    
    * task-2 - removed unnecessary title
    
    Co-authored-by: Ivan Menshchikov <ivan_menshchikov@vanguard.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -23,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'images'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('images');
+    expect(app.appTitle3).toEqual('images');
   });
 
   it('should render title', () => {
