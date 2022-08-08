ff74db94d16efc068e5bb9a5043747ee41e9182c
Ivan Menshchikov <m.ectuser@gmail.com>
2022-08-08 12:28:03 +0200

    task-4 - migrated from karma to jest (#5)
    Co-authored-by: Ivan Menshchikov <ivan_menshchikov@vanguard.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'images'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.appTitle3).toEqual('images');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('images app is running!');
-  });
 });
