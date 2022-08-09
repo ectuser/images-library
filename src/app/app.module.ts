3d0cefde2113fc7e1068ba200dbc6661a60124c0
Ivan Menshchikov <ivan_menshchikov@vanguard.com>
2022-08-09 10:34:37 +0200

    task-19 - provided interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutComponent } from './core/layout/layout.component';
 import { HeaderComponent } from './core/header/header.component';
 import { MaterialModule } from './material.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { PhotosModule } from './features/photos/photos.module';
 import { CONFIG_TOKEN } from './core/injection-tokens/config';
 import { environment } from '../environments/environment';
 import { RouterModule } from '@angular/router';
+import { HandleErrorInterceptor } from './core/interceptors/handle-error.interceptor';
 
 @NgModule({
   declarations: [AppComponent, LayoutComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule, HttpClientModule, PhotosModule, RouterModule],
-  providers: [{ provide: CONFIG_TOKEN, useValue: environment }],
+  providers: [
+    { provide: CONFIG_TOKEN, useValue: environment },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HandleErrorInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
