d4fae7e49f8ddbab84d02eaf0e2313ade2670731
Ivan Menshchikov <m.ectuser@gmail.com>
2022-08-10 01:59:22 +0200

    task-43 - added random delay (#44)
    Co-authored-by: Ivan Menshchikov <ivan_menshchikov@vanguard.com>

--- src/app/features/photos/photos-api.service.ts
+++ src/app/features/photos/photos-api.service.ts
@@ -4,6 +4,7 @@ import { CONFIG_TOKEN } from '../../core/injection-tokens/config';
 import { Config } from '../../../environments/config.model';
 import { PhotoResponse } from './photo.model';
 import { delay, Observable } from 'rxjs';
+import { randomNumber } from '../../core/utils/random-number';
 
 const numberOrImages = 6;
 
@@ -14,6 +15,8 @@ export class PhotosApiService {
   constructor(@Inject(CONFIG_TOKEN) private config: Config, private httpClient: HttpClient) {}
 
   getImages(): Observable<PhotoResponse> {
-    return this.httpClient.get<PhotoResponse>(this.config.apiEndpoints.photos + `/random/${numberOrImages}`).pipe(delay(300));
+    return this.httpClient
+      .get<PhotoResponse>(this.config.apiEndpoints.photos + `/random/${numberOrImages}`)
+      .pipe(delay(randomNumber(200, 300)));
   }
 }
