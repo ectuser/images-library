b3f52023cd42d5c5f649cc6a5410df147b603874
Ivan Menshchikov <ivan_menshchikov@vanguard.com>
2022-08-09 23:20:02 +0200

    task-37 - added tests for photo card component

--- src/app/shared/infinite-scroll/infinite-scroll.component.spec.ts
+++ src/app/shared/infinite-scroll/infinite-scroll.component.spec.ts
@@ -2,6 +2,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InfiniteScrollComponent } from './infinite-scroll.component';
 
+const mock = class IntersectionObserver {
+  observe() {
+    return null;
+  }
+
+  disconnect() {
+    return null;
+  }
+
+  unobserve() {
+    return null;
+  }
+};
+
+Object.defineProperty(window, 'IntersectionObserver', { value: mock });
+
 describe('InfiniteScrollComponent', () => {
   let component: InfiniteScrollComponent;
   let fixture: ComponentFixture<InfiniteScrollComponent>;
--- src/app/shared/photo-card/photo-card.component.spec.ts
+++ src/app/shared/photo-card/photo-card.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { PhotoCardComponent } from './photo-card.component';
 
@@ -8,7 +9,7 @@ describe('PhotoCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PhotoCardComponent ]
+      imports: [PhotoCardComponent],
     })
     .compileComponents();
 
@@ -19,5 +20,23 @@ describe('PhotoCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('p tag', () => {
+    it('should print "Favorite!" if the photo is favorite', () => {
+      component.isFavorite = true;
+
+      const pTag = fixture.debugElement.query(By.css('p'));
+
+      expect((pTag.nativeElement as HTMLElement).textContent === 'Favorite!');
+    });
+
+    it('should print "Add to favorite" if the photo is not favorite', () => {
+      component.isFavorite = false;
+
+      const pTag = fixture.debugElement.query(By.css('p'));
+
+      expect((pTag.nativeElement as HTMLElement).textContent === 'Add to favorite');
+    });
   });
 });
