bfb652165058f65201d4d03ec1efa6f7cce37196
Ivan Menshchikov <ivan_menshchikov@vanguard.com>
2022-08-09 23:01:43 +0200

    task-37 - added single photo component tests

--- src/app/features/photos/single-photo/single-photo.component.spec.ts
+++ src/app/features/photos/single-photo/single-photo.component.spec.ts
@@ -1,6 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { appRoutes } from '../../../app-routing.module';
+import { FavoritesService } from '../../favorites/favorites.service';
 
 import { SinglePhotoComponent } from './single-photo.component';
+
+class FavoritesServiceMock {
+  get favorites$(): Observable<string[]> {
+    return of(['one', 'two']);
+  }
+
+  removeFavorite = jest.fn();
+}
+
+const paramsSubject$ = new BehaviorSubject({id: 'two'});
 
 describe('SinglePhotoComponent', () => {
   let component: SinglePhotoComponent;
@@ -8,7 +23,12 @@ describe('SinglePhotoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SinglePhotoComponent ]
+      imports: [RouterTestingModule.withRoutes(appRoutes)],
+      declarations: [ SinglePhotoComponent ],
+      providers: [
+        { provide: FavoritesService, useClass: FavoritesServiceMock },
+        {provide: ActivatedRoute, useValue: {params: paramsSubject$ }}
+      ]
     })
     .compileComponents();
 
@@ -19,5 +39,50 @@ describe('SinglePhotoComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should extract url from route', (done) => {
+    component.url$.subscribe((url) => {
+      expect(url).toBe('two');
+      done();
+    });
+  });
+
+  it('should return set is favorite true if url is inside of favorites list', (done) => {
+    paramsSubject$.next({id: 'two'});
+
+    component.isFavorite$.subscribe((val) => {
+      expect(val).toBe(true);
+      done();
+    });
+  });
+
+  it('should return set is favorite false if favorites list does not include url', (done) => {
+    paramsSubject$.next({id: 'three'});
+
+    component.isFavorite$.subscribe((val) => {
+      expect(val).toBe(false);
+      done();
+    });
+  });
+
+  describe('removeFromFavorites', () => {
+    it('should remove favorite', () => {
+      paramsSubject$.next({id: 'three'});
+
+      const favServiceSpy = jest.spyOn(component['favoritesService'], 'removeFavorite');
+
+      component.removeFromFavorites();
+
+      expect(favServiceSpy).toHaveBeenCalledWith('three');
+    });
+
+    it('should navigate to favorites', () => {
+      const favServiceSpy = jest.spyOn(component['router'], 'navigate');
+
+      component.removeFromFavorites();
+
+      expect(favServiceSpy).toHaveBeenCalledWith(['/favorites']);
+    });
   });
 });
